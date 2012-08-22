baf8099a245d7a9683168fc0c011c00dac94b92b
tangxm <tangxm90@gmail.com>
2012-08-22 15:28:33 +0800

    CRUD操作

--- js/database.js
+++ js/database.js
@@ -12,24 +12,26 @@ void function(){
 	 * @param {Array}  fileds 表的字段
 	 */
 	var Table = function(name,fields){
+		
 		this.db = DB;
 		this.name = name;
 		this.fields = fields;
 
 		// 生成values(?,....?,?)。为插入数据做准备。
 		this.values = "?" + new Array(fields.length).join(",?");
-
 		// 创建 表。
 		this.executeSQL("create table "+name+" ("+fields.join(",")+")",[]);
 	};
 
 	Table.prototype = {
 
+		// 插入一条数据
 		insert:function(parameters){
 			var sql = "insert into "+this.name+" ("+this.fields.join(",")+") values("+this.values+")";
 			this.executeSQL(sql,parameters);
 		},
 
+		// 查询数据 by SQL 或者默认 查询整个表
 		query: function(sql,callback){
 			if(typeof sql === "function"){
 				callback = sql;
@@ -42,6 +44,7 @@ void function(){
 			});
 		},
 
+		// 执行SQL语句
 		executeSQL:function(sql,parameters,success,error){
 
 			this.db.transaction(function(tx){
@@ -49,6 +52,7 @@ void function(){
 			});
 		},
 
+		// 保存一个实例 进入数据库
 		save: function(instance){
 			var parameters = [];
 			this.fields.forEach(function(name){
@@ -57,16 +61,55 @@ void function(){
 			this.insert(parameters);
 		},
 
+		// 从数据删除一个实例
 		remove: function(instance){
 			this.executeSQL("delete from "+this.name+" where id = "+ instance.id)
+		},
+
+		// 更新数据库数据
+		update: function(instance){
+			var str = "";
+			for(var o in instance){
+				str += o == "id" ? "" : o + "='" + instance[o]+"',";
+			}
+			str = str.slice(-1) === "," ? str.slice(0,-1) : str;
+			this.executeSQL("update "+this.name+" set "+ str +" where id="+instance.id);
+		},
+
+		// 通过数组获得一个具体实例 var user = User.getInstance([1,"aki",19]);
+		// user --> {id:1, name: "aki", age:19} 
+		getInstance: function(values){
+			var instance = {};
+			this.fields.forEach(function(name,i){
+				instance[name] = values[i];
+			});
+			return instance;
 		}
+
 	};
 
 	var b = new Table("test",["id","name","age"]);
-	var instance = {id:1,name:'i',age:29};
+	var instance = {id:1,name:'iiiiupdate',age:2};
+	b.update(instance);
 	b.query("select * from test",function(tests){
 		var obj = tests[tests.length-1];
+		alert(obj.name);
 	});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
